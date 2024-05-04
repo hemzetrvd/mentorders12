64b8b75dbc6242e2fe630383d16a9e22134e4d12
Hemze <hmztarverdiyev@gmail.com>
2024-05-04 13:25:03 +0400

    firstlesson

---
+++ mentorder1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mentorder1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }int a = 2;
+    }
+}
